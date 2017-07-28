4a823d4fe8f5a805a5072c59f4f49c0b9560e6e4
Jeremy Lott <jlott007@gmail.com>
2017-07-28 09:00:37 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WTW.Controllers;
+using WTW.DI;
+using WTW.Interfaces;
+using WTW.Models;
+
+namespace wtw
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			Configure();
+		}
+
+		private void Configure()
+		{
+			IContainer container = JerDIContainer.GetInstance();
+			container.Register<IController, UsersController>(LifeCycleType.Singleton);
+			container.Register<IEmailService, EmailService>(LifeCycleType.Singleton);
+			container.Register<ICalculator, Calculator>();
+
+			var controller = container.Resolve<IController>();
+			var emailService = container.Resolve<IEmailService>();
+			var calculator = container.Resolve<ICalculator>();
+		}
+	}
+}
